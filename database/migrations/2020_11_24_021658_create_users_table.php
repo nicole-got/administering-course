5d5b57893d9510149d56309b9ecdf10fcb6ffb9c
unknown <nicole73oliveira@gmail.com>
2020-11-23 23:39:18 -0300

    add migration user

---
+++ database/migrations/2020_11_24_021658_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateUsersTable.
+ */
+class CreateUsersTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table) {
+			$table->increments('user_id');
+			$table->string('name', 60);
+			$table->string('email', 100)->unique();
+			$table->string('password', 50);
+			$table->string('phone', 15);
+			$table->set('genero', ['feminino', 'masculino','outros'])->nullable();
+			$table->set('status',[1,0])->default(1);
+			$table->dateTime('created',0)->nullable();
+
+            $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+}
